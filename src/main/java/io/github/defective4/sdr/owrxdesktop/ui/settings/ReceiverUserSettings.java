cee1458cd092c4719400e5e10517d9ee6639bae1
Defective <def3ctive4@gmail.com>
2026-03-13 13:56:10 +0100

    Load built-in themes from a resource file

--- src/main/java/io/github/defective4/sdr/owrxdesktop/ui/settings/ReceiverUserSettings.java
+++ src/main/java/io/github/defective4/sdr/owrxdesktop/ui/settings/ReceiverUserSettings.java
@@ -1,18 +1,54 @@
 package io.github.defective4.sdr.owrxdesktop.ui.settings;
 
+import java.awt.Color;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import io.github.defective4.sdr.owrxdesktop.ui.settings.waterfall.BuiltinWaterfallTheme;
 import io.github.defective4.sdr.owrxdesktop.ui.settings.waterfall.WaterfallThemeMode;
 
 public class ReceiverUserSettings {
+
     private BuiltinWaterfallTheme selectedBuiltinWaterfallTheme = BuiltinWaterfallTheme.TURBO;
+
+    private final Map<String, Color[]> themeIndex = new HashMap<>();
+
     private Set<String> waterfallCustomTheme = Set.of("#000000", "#ffffff");
     private WaterfallThemeMode waterfallThemeMode = WaterfallThemeMode.SERVER;
 
+    public ReceiverUserSettings() {
+        try (Reader reader = new InputStreamReader(getClass().getResourceAsStream("/themes.json"))) {
+            JsonObject obj = JsonParser.parseReader(reader).getAsJsonObject();
+            for (Entry<String, JsonElement> entry : obj.entrySet()) {
+                if (entry.getValue() instanceof JsonArray array) {
+                    String key = entry.getKey();
+                    Color[] colors = array.asList().stream().map(el -> Color.decode(el.getAsString()))
+                            .toArray(Color[]::new);
+                    themeIndex.put(key, colors);
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public BuiltinWaterfallTheme getSelectedBuiltinWaterfallTheme() {
         return selectedBuiltinWaterfallTheme;
+    }
+
+    public Optional<Color[]> getTheme(String index) {
+        return Optional.ofNullable(themeIndex.get(index));
     }
 
     public Set<String> getWaterfallCustomTheme() {
