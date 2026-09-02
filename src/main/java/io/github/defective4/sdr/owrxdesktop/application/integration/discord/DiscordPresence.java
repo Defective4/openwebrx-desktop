d85b84aa8a2c70f57f674a0ac688b9f07e3cf78b
Defective <def3ctive4@gmail.com>
2026-09-02 13:36:44 +0200

    Improve Discord integration

--- src/main/java/io/github/defective4/sdr/owrxdesktop/application/integration/discord/DiscordPresence.java
+++ src/main/java/io/github/defective4/sdr/owrxdesktop/application/integration/discord/DiscordPresence.java
@@ -2,6 +2,8 @@ package io.github.defective4.sdr.owrxdesktop.application.integration.discord;
 
 import java.text.ParseException;
 import java.time.Instant;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.jcm.discordgamesdk.ActivityManager;
 import de.jcm.discordgamesdk.Core;
@@ -19,13 +21,17 @@ public class DiscordPresence {
     private static final long APP_ID = 1544459542409117776L;
     private static final String DEFAULT_RECEIVER_NAME = "<No name>";
     private static final String LOGO_KEY = "logo";
+    private Activity activity;
     private final ActivityManager actManager;
     private final Core core;
+
     private final FrequencyFormatter frequencyFormatter = new FrequencyFormatter();
+    private long lastPresenceUpdate = 0;
+
+    private final Object lock = new Object();
 
     private final long timestamp = System.currentTimeMillis();
     private final ApplicationWindow window;
-
     public DiscordPresence(ApplicationWindow window) {
         this.window = window;
         CreateParams params = new CreateParams();
@@ -33,18 +39,27 @@ public class DiscordPresence {
         params.setClientID(APP_ID);
         core = new Core(params);
         actManager = core.activityManager();
+        new Timer(true).scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    setActivity();
+                }
+            }
+        }, 0, 5000);
     }
 
     public void updatePresence() {
-        Activity act = new Activity();
-        act.setInstance(true);
-        act.setType(ActivityType.PLAYING);
-        act.setDetails("In server list");
-        act.timestamps().setStart(Instant.ofEpochMilli(timestamp));
+        Activity activity = new Activity();
+        activity.setInstance(true);
+        activity.setType(ActivityType.PLAYING);
+        activity.setDetails("In server list");
+        activity.timestamps().setStart(Instant.ofEpochMilli(timestamp));
 
         if (window.getReceiver().isPresent()) {
             RadioReceiver rx = window.getReceiver().get();
-            act.setDetails(rx.getReceiverDetails().map(ReceiverDetails::receiverName).orElse(DEFAULT_RECEIVER_NAME));
+            activity.setDetails(
+                    rx.getReceiverDetails().map(ReceiverDetails::receiverName).orElse(DEFAULT_RECEIVER_NAME));
             if (rx.isConnected()) {
                 ReceiverWindow rxWindow = rx.getRxWindow();
                 String freq;
@@ -55,19 +70,31 @@ public class DiscordPresence {
                 }
                 ReceiverMode mode = rxWindow.getSecondaryMode().orElse(rxWindow.getPrimaryMode());
                 String fmt = "%s %s".formatted(freq, (mode == null ? ReceiverMode.EMPTY : mode).name());
-                act.setState(fmt);
+                activity.setState(fmt);
             } else {
-                act.setState("Connecting...");
+                activity.setState("Connecting...");
             }
         } else if (window.getAppState().changingSettings) {
-            act.setState("In settings");
+            activity.setState("In settings");
         } else {
-            act.setState(null);
+            activity.setState(null);
         }
 
-        act.assets().setLargeImage(LOGO_KEY);
-        act.assets().setLargeText("OpenWebRX Desktop");
+        activity.assets().setLargeImage(LOGO_KEY);
+        activity.assets().setLargeText("OpenWebRX Desktop");
 
-        actManager.updateActivity(act);
+        synchronized (lock) {
+            this.activity = activity;
+            if (System.currentTimeMillis() - lastPresenceUpdate > 5000) {
+                setActivity();
+            }
+        }
+    }
+
+    private void setActivity() {
+        if (activity == null) return;
+        actManager.updateActivity(activity);
+        activity = null;
+        lastPresenceUpdate = System.currentTimeMillis();
     }
 }
